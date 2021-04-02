320e12cdff73a5c68c5caf66ec600426e275e061
weilunhuang@PC <weilunhuang1@gmail.com>
2021-04-01 21:15:54 -0400

    add code to test for lumo set-up

--- codes/test_lumo.py
+++ codes/test_lumo.py
@@ -9,6 +9,7 @@ import logging
 import numpy as np
 import cv2
 import torch
+from collections import OrderedDict
 
 import utils.util as util
 import data.util as data_util
@@ -18,76 +19,70 @@ import argparse
 
 
 def main():
-    
+
     # Create object for parsing command-line options
-    parser = argparse.ArgumentParser(description="Test with EDVR, requre path to test dataset folder.")
-    # Add argument which takes path to a bag file as an input
+    parser = argparse.ArgumentParser(description="Test with EDVR, require path to test dataset folder.")
     parser.add_argument("-i", "--input", type=str, help="Path to test folder")
+    parser.add_argument("-m", "--model_mode", type=str, default="reds_m", help="model mode")
+    parser.add_argument("-p", "--model_path", type=str,  help="model path")
     # Parse the command line arguments to an object
     args = parser.parse_args()
+
     # Safety if no parameter have been given
-    if not args.input:
+    if args.input is None or args.model_path is None:
         print("No input paramater have been given.")
         print("For help type --help")
         exit()
 
     folder_name = args.input.split("/")[-1]
-    if folder_name == '':
-        index = len(args.input.split("/")) - 2
-        folder_name = args.input.split("/")[index]
+    # if folder_name == '':
+    #     index = len(args.input.split("/")) - 2
+    #     folder_name = args.input.split("/")[index]
+    model_mode = args.model_mode # reds_m | reds_l | vimeo
+    model_path = args.model_path
 
     #################
     # configurations
     #################
     device = torch.device('cuda')
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    data_mode = 'Vid4'  # Vid4 | sharp_bicubic | blur_bicubic | blur | blur_comp
-    # Vid4: SR
-    # REDS4: sharp_bicubic (SR-clean), blur_bicubic (SR-blur);
-    #        blur (deblur-clean), blur_comp (deblur-compression).
+     
     stage = 1  # 1 or 2, use two stage strategy for REDS dataset.
     flip_test = False
     ############################################################################
     #### model
-    if data_mode == 'Vid4':
-        if stage == 1:
-            model_path = '../experiments/pretrained_models/EDVR_Vimeo90K_SR_L.pth'
-        else:
-            raise ValueError('Vid4 does not support stage 2.')
-    else:
-        raise NotImplementedError
-
-    if data_mode == 'Vid4':
-        N_in = 7  # use N_in images to restore one HR image
-    else:
-        N_in = 5
-
+    # hyper params, default for reds_m model
+    N_f = 64
+    N_in = 5
     predeblur, HR_in = False, False
-    back_RBs = 40
+    back_RBs = 10
 
-    model = EDVR_arch.EDVR(128, N_in, 8, 5, back_RBs, predeblur=predeblur, HR_in=HR_in)
+    # change for large model
+    if model_mode == "vimeo" or model_mode == "reds_l":
+        N_f = 128
+        back_RBs = 40
+        if model_mode == 'vimeo':
+            N_in = 7  # use N_in images to restore one HR image
+        else:
+            N_in = 5
+
+    if stage == 2:
+        HR_in = True
+        back_RBs = 20
+
+    # create model    
+    model = EDVR_arch.EDVR(N_f, N_in, 8, 5, back_RBs, predeblur=predeblur, HR_in=HR_in)
 
     #### dataset
-    if data_mode == 'Vid4':
-        # debug
-        test_dataset_folder = os.path.join(args.input, 'BIx4')
-        GT_dataset_folder = os.path.join(args.input, 'GT')
-    else:
-        if stage == 1:
-            test_dataset_folder = '../datasets/REDS4/{}'.format(data_mode)
-        else:
-            test_dataset_folder = '../results/REDS-EDVR_REDS_SR_L_flipx4'
-            print('You should modify the test_dataset_folder path for stage 2')
-        GT_dataset_folder = '../datasets/REDS4/GT'
+    test_dataset_folder = os.path.join(args.input, 'val_d4')
+    GT_dataset_folder = os.path.join(args.input, 'val_gt')
 
     #### evaluation
     crop_border = 0
     border_frame = N_in // 2  # border frames when evaluate
     # temporal padding mode
-    if data_mode == 'Vid4' or data_mode == 'sharp_bicubic':
-        padding = 'new_info'
-    else:
-        padding = 'replicate'
+    padding = 'new_info'
+
     save_imgs = True
 
     save_folder = '../results/{}'.format(folder_name)
@@ -103,7 +98,17 @@ def main():
     logger.info('Flip test: {}'.format(flip_test))
 
     #### set up the models
-    model.load_state_dict(torch.load(model_path), strict=True)
+    print(model)
+    model.load_state_dict(torch.load(model_path), strict=False)
+    # load_net = torch.load(model_path)
+    # load_net_clean = OrderedDict()  # remove unnecessary 'module.'
+    # for k, v in load_net.items():
+    #     if k.startswith('module.'):
+    #         load_net_clean[k[7:]] = v
+    #     else:
+    #         load_net_clean[k] = v
+    # model.load_state_dict(load_net_clean, strict=True)
+
     model.eval()
     model = model.to(device)
 
@@ -114,6 +119,7 @@ def main():
     subfolder_GT_l = sorted(glob.glob(osp.join(GT_dataset_folder, '*')))
     # for each subfolder
     for subfolder, subfolder_GT in zip(subfolder_l, subfolder_GT_l):
+
         subfolder_name = osp.basename(subfolder)
         subfolder_name_l.append(subfolder_name)
         save_subfolder = osp.join(save_folder, subfolder_name)
@@ -135,8 +141,9 @@ def main():
         for img_idx, img_path in enumerate(img_path_l):
             img_name = osp.splitext(osp.basename(img_path))[0]
             select_idx = data_util.index_generation(img_idx, max_idx, N_in, padding=padding)
-            # print(select_idx)
             imgs_in = imgs_LQ.index_select(0, torch.LongTensor(select_idx)).unsqueeze(0).to(device)
+            # imgs_in = imgs_LQ.index_select(0, torch.LongTensor(select_idx))
+            # print(imgs_in.shape)
 
             if flip_test:
                 output = util.flipx4_forward(model, imgs_in)
@@ -151,7 +158,7 @@ def main():
             output = output / 255.
             GT = np.copy(img_GT_l[img_idx])
             # For REDS, evaluate on RGB channels; for Vid4, evaluate on the Y channel
-            if data_mode == 'Vid4':  # bgr2y, [0, 1]
+            if model_mode == 'vimeo':  # bgr2y, [0, 1]
                 GT = data_util.bgr2ycbcr(GT, only_y=True)
                 output = data_util.bgr2ycbcr(output, only_y=True)
 
@@ -198,7 +205,6 @@ def main():
                     sum(avg_psnr_l) / len(avg_psnr_l), len(subfolder_l),
                     sum(avg_psnr_center_l) / len(avg_psnr_center_l),
                     sum(avg_psnr_border_l) / len(avg_psnr_border_l)))
-
 
 if __name__ == '__main__':
     main()
